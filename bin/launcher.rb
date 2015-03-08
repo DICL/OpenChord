7dedf13e55ec594ed6dbb415a5ff7faaf029f56b
Vicente Adolfo Bolea Sanchez <vicente.bolea@gmail.com>
2015-03-09 00:07:11 +0900

    improving

--- bin/launcher.rb
+++ bin/launcher.rb
@@ -3,4 +3,4 @@ load 'openchord.rb'
 include OpenChord
 
 raise 'ECLIPSE_PATH env variable is not defined' unless ENV["ECLIPSE_PATH"]
-Launcher.new  argv: ARGV, filepath: ENV["ECLIPSE_PATH"] + "/etc/eclipse.json"
+CLIcontroler.new input: ARGV, filepath: ENV["ECLIPSE_PATH"] + "/etc/eclipse.json"
--- bin/openchord.rb
+++ bin/openchord.rb
@@ -6,52 +6,11 @@ module OpenChord
   class ::String #{{{
     def warnout; warn self; end; 
   end #}}}
-  class CLIcontroler < Launcher #{{{
-    options = {}
-    def initialize (input)
-      OptionParser.new do |opts|
-        opts.banner = "Usage: openchord.rb [options]"
-
-        opts.on("-s", "--stat", "Run verbosely") do |i| 
-          ochord.info 
-        end 
-
-        opts.on("-cAddress", "--create=Address", "Run verbosely") do |i| 
-          options[:address] = i 
-        end 
-
-        opts.on("-jAddress", "--join=Address", "Run verbosely") do |i| 
-          options[:address] = i 
-        end 
-
-        opts.on("-i", "--insert", "Run verbosely") do |i| 
-        #  options[:verbose] = i 
-        end 
-
-        opts.on("-r", "--retrieve", "Run verbosely") do |i| 
-         # options[:verbose] = i 
-        end 
-
-        opts.on("-d", "--delete", "Run verbosely") do |i| 
-         # options[:verbose] = i 
-        end 
-
-        opts.on("-k", "--close", "Run verbosely") do |i| 
-          options[:verbose] = i 
-        end 
-
-        opts.on("-K", "--hardclose", "Run verbosely") do |i| 
-          options[:verbose] = i 
-        end 
-      end.parse! input
-      p options
-    end 
-  end #}}}
 
   class Launcher
     # initialize {{{
     #
-    def initialize argv:, filepath: 
+    def initialize filepath: 
       # Create the dictionary to call the OpenChord routines
       # Overkill right? but it was supposed to have many elements at the beginning
       @@chordcmd = %i[create join].map do |k| 
@@ -61,23 +20,51 @@ module OpenChord
 
       # Load configuration JSON file
       File.open(filepath, 'rb') { |f| @nodelist = JSON.parse(f.read) }
-  
-      # Run the commands
-      fail "No commands specified" unless argv.any? 
-      if argv.first == "insert"
-        insert key: argv[1], value: argv[2]             # Case for inserting
-      else              
-        send argv.first                                 # Case for creating or join
-      end
-  
+
     rescue => e
       ( { "Errno::ENOENT" => "File not found, change filepath",
           "NoMethodError" => "Wrong options passed to the program" 
       } [e.class.name] or e.message).red.warnout
-      abort HELP
     end
 
     # }}} 
+    # create {{{
+    #
+    def create
+      @pidlist['master'] = `#{@@chordcmd[:create]} #{@nodelist['master_address']} &> /dev/null & echo $!`.chomp  # Run master
+
+      @nodelist['nodes'].each do |node|
+        @pidlist[node] = `ssh #{node} '#{@@chordcmd[:join]} #{@nodelist['master_address']} &> /dev/null & echo $!' `.chomp
+      end
+
+      File.open('ochord.pid', 'w') { |f| f.write JSON.generate(@pidlist) }
+      ap @pidlist
+    end
+  end #}}}
+    # close {{{
+    #
+    def close
+      @pidlist= File.open('ochord.pid', 'r') { |f| JSON.parse(f.read) }  # Assert that we have a pidfile
+
+      `kill #{@pidlist['master']}`                                       # Kill master
+      @nodelist['nodes'].each do |node|                                  # Kill for each of the nodes
+        `ssh #{node} kill #{@pidlist[node]}` 
+      end
+
+      File.delete 'ochord.pid'
+    end
+
+    # }}}
+    # hard_close {{{
+    #
+    def hardclose
+      `pkill -u vicente java`                                              # Kill master
+      @nodelist['nodes'].each do |node|                                    # Kill for each of the nodes
+        `ssh #{node} pkill -u vicente java` 
+      end
+    end
+
+    # }}}
     # info {{{
     def info 
       ap File.open('ochord.pid', 'r') { |f| JSON.parse(f.read) } if File.exist? 'ochord.pid'
@@ -97,41 +84,24 @@ module OpenChord
     end
 
     # }}}
-    # close {{{
-    #
-    def close
-      @pidlist= File.open('ochord.pid', 'r') { |f| JSON.parse(f.read) }  # Assert that we have a pidfile
 
-      `kill #{@pidlist['master']}`                                       # Kill master
-      @nodelist['nodes'].each do |node|                                  # Kill for each of the nodes
-        `ssh #{node} kill #{@pidlist[node]}` 
-      end
+  class CLIcontroler < Launcher
+    def initialize input:, filepath:  #{{{
+      @options = {}
+      super(filepath: filepath)
 
-      File.delete 'ochord.pid'
-    end
-  
-    # }}}
-    # hard_close {{{
-    #
-    def hardclose
-      `pkill -u vicente java`                                              # Kill master
-      @nodelist['nodes'].each do |node|                                    # Kill for each of the nodes
-        `ssh #{node} pkill -u vicente java` 
-      end
-    end
+      OptionParser.new do |opts|
+        opts.banner = "Usage: openchord.rb [options]"
 
-    # }}}
-    # create {{{
-    #
-    def create
-      @pidlist['master'] = `#{@@chordcmd[:create]} #{@nodelist['master_address']} &> /dev/null & echo $!`.chomp  # Run master
-
-      @nodelist['nodes'].each do |node|
-        @pidlist[node] = `ssh #{node} '#{@@chordcmd[:join]} #{@nodelist['master_address']} &> /dev/null & echo $!' `.chomp
-      end
-
-      File.open('ochord.pid', 'w') { |f| f.write JSON.generate(@pidlist) }
-      ap @pidlist
-    end
-  end
-end 
+        opts.on("-s", "--stat"        , "Reveal current setting") { |i| info }
+        opts.on("-c", "--create [Address]", "Create new openchord network") { |i| @options[:address] = i; create }
+        opts.on("-i key,value", "--insert key,value", Array, "insert new field") { |i| }
+        opts.on("-r", "--retrieve key", "Retrieve existing field") { |i| }
+        opts.on("-d", "--delete key"  , "delete a field") { |i| }
+        opts.on("-k", "--close"       , "close network")  { |i| @options[:verbose] = i; close }
+        opts.on("-K", "--hardclose"   , "no mercy close") { |i| hardclose }
+        opts.on("-h", "--help"        , "recursive this") { |i| puts opts }
+      end.parse! input
+    end 
+  end #}}}
+end
